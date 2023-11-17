80719b1941145d3f8fcb5b6804eebd91582c2859
najamoe <najamoe@outlook.dk>
2023-11-17 10:47:49 +0100

    Initial

---
+++ src/main/java/com/example/bookingsystembackend/BookingSystemBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.bookingsystembackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingSystemBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingSystemBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bookingsystembackend/BookingSystemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bookingsystembackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingSystemBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
