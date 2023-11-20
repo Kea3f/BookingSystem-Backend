4dd254412d383e6bc1598dada7a4c8aec50a55cb
Najamoe <naja0718@stud.kea.dk>
2023-11-20 12:39:25 +0100

    Programme working again

--- src/main/java/com/example/bookingsystembackend/entity/Client.java
+++ src/main/java/com/example/bookingsystembackend/entity/Client.java
@@ -1,9 +1,6 @@
 package com.example.bookingsystembackend.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.NoArgsConstructor;
