6cc01f6ee7ecbfcda18fad6160aae91df30f70ab
Najamoe <naja0718@stud.kea.dk>
2023-11-21 14:03:59 +0100

    CRUD operations started in TreatmentService, TreatmentRepository - Starting on the controller logic

--- src/main/java/com/example/bookingsystembackend/controller/TreatmentController.java
+++ src/main/java/com/example/bookingsystembackend/controller/TreatmentController.java
@@ -1,12 +1,35 @@
 package com.example.bookingsystembackend.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.bookingsystembackend.entity.Treatment;
+import com.example.bookingsystembackend.service.TreatmentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/api/treatment")
 public class TreatmentController {
 
+    private final TreatmentService treatmentService;
+    @Autowired
+    public TreatmentController(TreatmentService treatmentService, TreatmentService treatmentService1){
+        this.treatmentService = treatmentService;
+    }
+
+    @PostMapping("/create")
+    public Treatment createTreatment(@RequestBody Treatment treatment){
+        return treatmentService.createTreatment(treatment);
+    }
+
+    @PutMapping("/edit")
+    public ResponseEntity<Treatment> editTreatment(@PathVariable int treatmentId, @RequestBody Treatment editTreatment){
+        Treatment treatment = treatmentService.editTreatment(treatmentId, editTreatment);
+        if (treatment != null){
+            return ResponseEntity.ok(treatment);
+        }
+        return ResponseEntity.noContent().build();
+    }
 
 
 }
