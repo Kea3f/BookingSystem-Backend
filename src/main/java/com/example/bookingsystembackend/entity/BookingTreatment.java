611302c58388a3c19b395b46e8764c38ad661456
najamoe <najamoe@outlook.dk>
2023-11-26 19:26:01 +0100

    Added new entity class for bookingtreatments in order for the database to show the correct connections between customer/booking/treatment

---
+++ src/main/java/com/example/bookingsystembackend/entity/BookingTreatment.java
@@ -0,0 +1,30 @@
+package com.example.bookingsystembackend.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Setter
+@Getter
+@NoArgsConstructor
+@Table(name = "booking_treatments")
+public class BookingTreatment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @ManyToOne
+    @JoinColumn(name = "booking_id")
+    private Booking booking;
+
+    @ManyToOne
+    @JoinColumn(name = "customer_id")
+    private Customer customer;
+
+    @ManyToOne
+    @JoinColumn(name = "treatment_id")
+    private Treatment treatment;
+}
