569e5da739fef0da2a8870687a77a4c784a282b1
najamoe <najamoe@outlook.dk>
2023-11-28 13:57:53 +0100

    deleted cors in controller

--- src/main/java/com/example/bookingsystembackend/controller/CustomerController.java
+++ src/main/java/com/example/bookingsystembackend/controller/CustomerController.java
@@ -15,7 +15,6 @@ import org.springframework.web.bind.annotation.*;
 
 
 @RestController
-@CrossOrigin(origins = "http://localhost:63342")
 @RequestMapping("/api/customer")
 public class CustomerController {
 
