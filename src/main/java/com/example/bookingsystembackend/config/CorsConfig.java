edc3089ebdd68abc65b9fa63123fda26d1a5934a
najamoe <najamoe@outlook.dk>
2023-11-29 11:40:23 +0100

    added bean for modelmapper

--- src/main/java/com/example/bookingsystembackend/config/CorsConfig.java
+++ src/main/java/com/example/bookingsystembackend/config/CorsConfig.java
@@ -1,5 +1,7 @@
 package com.example.bookingsystembackend.config;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -14,5 +16,10 @@ public class CorsConfig implements WebMvcConfigurer {
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowCredentials(true)
                 .maxAge(3600);
+    }
+
+    @Bean
+    public ModelMapper modelMapper(){
+        return new ModelMapper();
     }
 }
