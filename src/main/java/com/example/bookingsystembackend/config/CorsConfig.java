d92edd11ae100a7e93635beaecff0cca78672adc
najamoe <najamoe@outlook.dk>
2023-12-05 10:44:42 +0100

    ModelMapper fixed

--- src/main/java/com/example/bookingsystembackend/config/CorsConfig.java
+++ src/main/java/com/example/bookingsystembackend/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package com.example.bookingsystembackend.config;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
